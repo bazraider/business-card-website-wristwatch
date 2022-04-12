ecf525327383f151b8bbfa6bd63e7f81221332ee
Dima <schtanckevitch.dima@yandex.ru>
2022-04-12 19:08:28 +0300

    dima2

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,13 @@
+const async = require('hbs/lib/async');
+
 const indexRouter = require('express').Router();
 
 indexRouter.get('/', async (req, res) => {
   res.render('index');
+});
+
+indexRouter.get('/cards', async (req, res) => {
+  res.render('cards');
 });
 
 module.exports = indexRouter;
