437d0f08dcd4f9dad52ac9e40722a611027fc803
Merdzhen <merdzhen.k@gmail.com>
2022-04-13 15:27:25 +0300

    delete product - for admins

--- public/js/admin.js
+++ public/js/admin.js
@@ -8,3 +8,27 @@ if (admin) {
   }
   // console.log(adminBtns[0]);
 }
+
+// удаление товара:
+const allProducts = document.querySelector('.js-products');
+
+allProducts?.addEventListener('click', async (event) => {
+  if (event.target.classList.contains('js-delete')) {
+    const id = event.target.id.slice(6);
+    // console.log('delete', id);
+    const response = await fetch('/products/delete', { // создаем роутер delete
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id,
+      }), // посылаем на ручку id, чтобы в базе по нему можно было удалить
+    });
+
+    // console.log('response del', response);
+    if (response.status === 200) { // будем удалять только если все ок и статус 200
+      allProducts.removeChild(event.target.parentNode.parentNode); // удаляем родительских элемент кнопки (маленький div) со страницы, но не из базы
+    }
+  }
+});
