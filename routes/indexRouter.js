dcc87cce36de45f1a591e51890a14d95d34301a9
Dima <schtanckevitch.dima@yandex.ru>
2022-04-14 10:11:29 +0300

    'new d'

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,18 +2,9 @@ const indexRouter = require('express').Router();
 const { Product } = require('../db/models');
 
 indexRouter.get('/', async (req, res) => {
-<<<<<<< HEAD
-  res.render('index');
-});
-
-// indexRouter.get('/cards', async (req, res) => {
-//   res.render('cards');
-// });
-=======
   const products = await Product.findAll({ raw: true });
   // console.log(products);
   res.render('index', { products });
 });
->>>>>>> 6b44e4d75adc8d85081e16b5a2dadc17883c2959
 
 module.exports = indexRouter;
