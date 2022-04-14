59540e420a9a517872b91104c22b91170f70259b
Liza <lizakorneva1997@gmail.com>
2022-04-14 14:42:45 +0300

    added picture table, added several pictures

--- routes/orderFormRouter.js
+++ routes/orderFormRouter.js
@@ -1,29 +1,37 @@
 const express = require('express');
+// const client = require('pg/lib/native/client');
 const db = require('../db/models');
 const multer = require('../middleware/multer.middleware');
 
 const router = express.Router();
 
-router.post('/', multer.single('img'), async (req, res) => {
-  // console.log(req.body.name);
-  // console.log(req.body.img);
-  const {
-    name, email, phone, message,
-  } = req.body;
-  const { path } = req.file;
-
+router.post('/', multer.array('img'), async (req, res) => {
   try {
-    await db.Client.create(
+  // console.log(name, email, phone, message);
+  // console.log(path);
+
+    const {
+      name, email, phone, message,
+    } = req.body;
+    // const { path } = req.files;
+
+    console.log(name, email, phone, message);
+    console.log(req.files[0].path);
+
+    const clientC = await db.Client.create(
       {
         name: name.toLowerCase(),
         email,
         phone,
-        img: path.slice(6),
         message,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
     );
+
+    await db.Picture.bulkCreate(req.files.map((file) => ({
+      img: file.path.slice(6), clientId: clientC.id, createdAt: new Date(), updatedAt: new Date(),
+    })));
 
     res.send({ success: true });
   } catch (error) {
