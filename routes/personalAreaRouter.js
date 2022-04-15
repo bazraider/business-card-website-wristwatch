16a62d28ff38cfd0dc427c0b0ca63fb10f702aa9
Liza <lizakorneva1997@gmail.com>
2022-04-15 09:59:58 +0300

    merge by main

--- routes/personalAreaRouter.js
+++ routes/personalAreaRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
 // const csvController = require('../public/js/csv.controller');
 const { append } = require('express/lib/response');
-const { Client } = require('../db/models');
+const { Client, Product } = require('../db/models');
 const multer = require('../middleware/multer.middleware');
-const CsvParser = require("json2csv").Parser;
+const CsvParser = require('json2csv').Parser;
 
 router.get('/', (req, res) => {
   res.render('admin/personalArea');
@@ -14,8 +15,12 @@ router.get('/download', async (req, res) => {
   const clients = await Client.findAll();
   const arrayClients = [];
   clients.forEach((client) => {
-    const { id, name, email, phone } = client;
-    arrayClients.push({ id, name, email, phone });
+    const {
+      id, name, email, phone,
+    } = client;
+    arrayClients.push({
+      id, name, email, phone,
+    });
   });
 
   const csvFields = ['id', 'name', 'email', 'phone'];
@@ -23,6 +28,26 @@ router.get('/download', async (req, res) => {
   const csvData = csvParser.parse(arrayClients);
   res.setHeader('Content-Type', 'text/csv');
   res.setHeader('Content-Disposition', 'attachment; filename=clients.csv');
+  res.status(200).end(csvData);
+});
+
+router.get('/downloadProducts', async (req, res) => {
+  const products = await Product.findAll();
+  const arrayProducts = [];
+  products.forEach((product) => {
+    const {
+      id, title, description, price,
+    } = product;
+    arrayProducts.push({
+      id, title, description, price,
+    });
+  });
+
+  const csvFields = ['id', 'title', 'description', 'price'];
+  const csvParser = new CsvParser({ csvFields });
+  const csvData = csvParser.parse(arrayProducts);
+  res.setHeader('Content-Type', 'text/csv');
+  res.setHeader('Content-Disposition', 'attachment; filename=products.csv');
   res.status(200).end(csvData);
 });
 
